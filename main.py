82f92a6e47cb3da73725166dc518c6ec3f46c7a4
mickizalan <71698357+mickizalan@users.noreply.github.com>
2022-01-18 12:36:19 +0000

    def szumma

--- main.py
+++ main.py
@@ -2,6 +2,13 @@
 2. Feladat
 Írj egy programot, amely a felhasználótól kér be egész számokat [-5;5] intervallumban. A bekérés akkor fejeződjön be, amikor a felhasználó intervallumon kívüli értéket ad meg! A program írja ki a megadott intervallumba eső számokat és az összegüket!
 '''
+def szumma(listacska):
+    osszeg = 0
+    for i in listacska:
+        osszeg += i
+
+    return osszeg
+
 
 lista = []
 
@@ -14,7 +21,4 @@ while True:
 
 print(lista)
 
-osszeg = 0
-for i in lista:
-    osszeg += i
-print(osszeg)
+print(szumma(lista))
